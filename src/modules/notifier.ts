0a878a47268946ab532a5a5570b2bc7dc9a54766
imshubhamgiri <skgiri569@gmail.com>
2026-06-02 20:44:22 +0530

    fix: add messageTemple to send daily stats to Telegram

--- src/modules/notifier.ts
+++ src/modules/notifier.ts
@@ -4,9 +4,21 @@ const bot = new Telegraf(process.env.TELEGRAM_TOKEN as string || '');
 
 
 
-export async function sendDailyReport(appliedJobs: ApplicationRecord[] , stats: DailyReportStats): Promise<void> {
-  const msg = appliedJobs.map(j =>
+export async function sendDailyReport(appliedJobs: ApplicationRecord[] , stats: DailyReportStats ): Promise<void> {
+  let messageTemplate
+  let msg = appliedJobs.map(j =>
     `✅ ${j.title} @ ${j.company}`
   ).join('\n');
+
+  messageTemplate = `
+ 📊 *Daily Job Report Stats* (${stats.jobsScraped})
+ ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣
+ 📝 *Total Applied:* ${stats.jobsApplied}
+ 📅 *Emails Sent:* ${stats.emailsSent}
+ -> *MatchedJobs:* ${stats.jobsMatched}
+ ⏳ *note:* ${stats.note}
+ `;
+ 
   await bot.telegram.sendMessage(process.env.CHAT_ID as string || '', msg);
+  await bot.telegram.sendMessage(process.env.CHAT_ID as string || '', messageTemplate, { parse_mode: 'Markdown' });
 }
